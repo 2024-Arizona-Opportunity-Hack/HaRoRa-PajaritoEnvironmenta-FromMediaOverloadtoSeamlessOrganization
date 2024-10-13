f2cf2753e9d616b56e37a0ac956769bfa5fac550
Rajat Pawar <rajat.pawar100@gmail.com>
2024-10-13 00:03:11 -0700

    added insert function

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -1,22 +1,30 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS postgis;
 
--- Table creation 
+-- Table creation
 CREATE TABLE image_detail (
                               uuid UUID PRIMARY KEY,
                               url TEXT,
                               title TEXT,
                               caption TEXT,
                               tags TEXT,
-                              title_caption_tags_fts_vector generated always as  (to_tsvector('english', COALESCE(tags, '') || ' ' || COALESCE(title, '') || ' ' || COALESCE(caption, ''))) stored.
+                              title_caption_tags_fts_vector tsvector generated always as  (to_tsvector('english', COALESCE(tags, '') || ' ' || COALESCE(title, '') || ' ' || COALESCE(caption, ''))) stored,
                               embedding_vector VECTOR(768),
                               coordinates GEOMETRY(POINT, 4326),
                               capture_time TIMESTAMP,
                               extended_meta JSON,
                               season TEXT,
-                              updated_at TIMESTAMP,
-                              created_at TIMESTAMP
+                              updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Index creation for searching on coordinates
 CREATE INDEX CONCURRENTLY idx_image_detail_coordinates ON image_detail USING GIST (coordinates);
