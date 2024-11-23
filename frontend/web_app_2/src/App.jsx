1fafd1c75f094228dbe5a33953f8ad326f415d27
Rohan Awhad <rohanavad007@gmail.com>
2024-11-22 19:38:43 -0700

    logged-in home page done; search results page done

--- frontend/web_app_2/src/App.jsx
+++ frontend/web_app_2/src/App.jsx
@@ -1,26 +1,85 @@
-import { useState } from 'react'
+import SearchBar from '@/components/Searchbar'
+import UploadIcon from '@/icons/UploadIcon'
+import LogoutIcon from '@/icons/LogoutIcon'
+import PencilIcon from '@/icons/PencilIcon'
 
 function App() {
+  const isAuthenticated = true;
+  const pressedSearch = true;
 
   return (
-    <div className='container mx-auto max-6xl h-svh overflow-hidden'>
-      <div className='grid grid-cols-12 grid-rows-5 gap-4 h-full'>
-        <div className='row-start-2 row-span-1 col-span-6 col-start-4 text-center font-grotesk font-bold text-6xl pt-16'>
-          PixQuery
-        </div>
-        <div className='row-start-3 row-span-1 col-span-8 col-start-3 justify-items-center font-grotesk font-bold text-6xl'>
-          <label className='w-9/12 h-1/3 rounded-full flex flex-row border bg-white px-3'>
-            {/* search icon */}
-              <svg className='h-6 w-6 my-auto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+    <div className='bg-base-100'>
 
-            {/* input query box */}
-              <input type='text' className='w-full placeholder:text-sm text-sm  bg-transparent grow focus:ring-0 focus:border-0 active:ring-0 active:border-0 mx-2 px-2' placeholder='Search'/>
+      {/* if logged in */}
+      {isAuthenticated && (
+        <>
 
-            {/* camera icon */}
-              <svg className='h-6 w-6 my-auto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M149.1 64.8L138.7 96 64 96C28.7 96 0 124.7 0 160L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-256c0-35.3-28.7-64-64-64l-74.7 0L362.9 64.8C356.4 45.2 338.1 32 317.4 32L194.6 32c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
-          </label>
-        </div>
-      </div>
+        {/* if logged in */}
+        {pressedSearch && (
+          <>
+            <div className='container mx-auto max-w-6xl'>
+                <div className='row-start-1 row-span-1 col-start-1 h-16 mt-6 col-span-12 grid grid-cols-6'>
+                  <div className='text-center font-grotesk font-bold text-2xl text-primary mt-1'>PixQuery</div>
+                  <div className='col-span-4 h-4/6'><SearchBar height='h-full'/></div>
+                  <div className='col-start-6 grid grid-cols-4 gap-2 mt-2'>
+                    <UploadIcon className='col-start-3'/>
+                    <LogoutIcon />
+                  </div>
+                </div>
+            </div>
+            <div className="divider h-px"></div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-2'>
+                {/* search results */}
+
+                <div className='text-sm'>
+                  <img className='rounded-lg' src='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/02/aoi-todo.jpg' />
+                  <div className='font-semibold mt-2'>Todo Angry</div> 
+                  <div className='grid-cols-8 grid gap-2'>
+                    <div className='col-span-7 truncate'><span className='font-semibold'>Tags: </span>Todo, Jujutsu Kaisen, Angry, Zone ...</div> 
+                    <div className='justify-items-end'><PencilIcon className='h-4 w-4 justify-items-end'/></div>
+                  </div>
+                </div>
+
+                {Array.from({ length: 20 }).map((_, index) => (
+                  <div key={index} className='text-sm'>
+                    <img className='rounded-lg' src='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/02/aoi-todo.jpg' />
+                    <div className='font-semibold mt-2'>Todo Angry</div>
+                    <div className='grid-cols-8 grid gap-2'>
+                      <div className='col-span-7 truncate'>
+                        <span className='font-semibold'>Tags: </span>Todo, Jujutsu Kaisen, Angry, Zone ...
+                      </div>
+                      <div className='justify-items-end'>
+                        <PencilIcon className='h-4 w-4 justify-items-end'/>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+            </div>
+
+          </>
+
+        )}
+
+        {!pressedSearch && (
+          <div className='container mx-auto max-w-7xl h-svh overflow-hidden'>
+            <div className='grid md:grid-cols-12 grid-cols-4 grid-rows-5 gap-4 h-full mt-4'>
+              <div className='row-start-1 col-start-4 col-span-1 md:col-start-11 md:col-span-2 lg:col-span-1 lg:col-start-12 justify-items-end lg:mr-4 mr-2 flex flex-row-reverse grid grid-cols-2 gap-6'>
+                <UploadIcon />
+                <LogoutIcon />
+              </div>
+
+              <div className='row-start-2 flex flex-col-reverse row-span-1 col-start-1 col-span-4 md:col-span-6 md:col-start-4 text-center font-grotesk font-bold text-6xl text-primary'>PixQuery</div>
+
+
+              <div className='row-start-3 row-span-1 col-start-1 col-span-4 md:col-span-8 md:col-start-3 justify-items-center font-grotesk font-bold text-6xl text-primary'>
+                  <SearchBar height={'h-1/3'}/>
+              </div>
+            </div>
+          </div>
+        )}
+        </>
+      )}
     </div>
   )
 }
